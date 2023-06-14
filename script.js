4caa316412f755290a30d232d46a5982a7775fbc
Darpan1211 <darpan.innovate@gmail.com>
2023-06-14 16:09:25 +0530

    js file learn

---
+++ script.js
@@ -0,0 +1,16 @@
+// STRINGS
+
+//   let firstName = "Darpan";
+//   let lastName = "Patel";
+//   console.log(firstName,lastName);
+
+//   //method 2 using template literals
+
+// let fullName = `I want to become ${firstName} and ${lastName}`
+// console.log(fullName[3]);
+
+//common string methods
+let hobbies = 'codding reading runnings';
+let result = hobbies.trim();
+console.log(hobbies.slice(0,5));
+
