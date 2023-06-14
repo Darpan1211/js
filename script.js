905e6386952293f18e7b950a985683a92a609bf5
Darpan1211 <darpan.innovate@gmail.com>
2023-06-14 16:36:28 +0530

    split conver

--- script.js
+++ script.js
@@ -1,16 +1,3 @@
-// STRINGS
-
-//   let firstName = "Darpan";
-//   let lastName = "Patel";
-//   console.log(firstName,lastName);
-
-//   //method 2 using template literals
-
-// let fullName = `I want to become ${firstName} and ${lastName}`
-// console.log(fullName[3]);
-
-//common string methods
-let hobbies = 'codding reading runnings';
-let result = hobbies.trim();
-console.log(hobbies.slice(0,5));
-
+let favoriteColors = "red blue green yellow"
+let arrcolor = favoriteColors.split(" ");
+console.log(arrcolor);
