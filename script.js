338555a311cc7b533ed4348140a4a575a9610588
Darpan1211 <darpan.innovate@gmail.com>
2023-06-15 16:14:54 +0530

    password verification project

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
-let favoriteColors = "red blue green yellow"
-let arrcolor = favoriteColors.split(" ");
-console.log(arrcolor);
+let password = "Darpan@1211";
+if (password.length>8 && password.includes("@")){
+    console.log("your password is very strong");
+}
+else(
+    console.log("your password is weak")
+)
