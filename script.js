d0fb7340e358e9c521b142c89789fa66ae7c3d56
Darpan1211 <darpan.innovate@gmail.com>
2023-06-16 17:19:36 +0530

    Bind Method

--- script.js
+++ script.js
@@ -1,25 +1,10 @@
-//Function
-"use strict"
-let mainPlane={
-    airline : "Fly India",
-    iatacode:"FI",
-    bookings:[],
-    book:function(flightNum,name){
-        console.log(`${name} Booking Flight on ${this.airline} with fight Number ${this.iatacode}${flightNum}`);
-        this.bookings.push({flightName:`${this.airline}`, name:name,flightNumber:flightNum});
-    }
+function greet(){
+    console.log(`Hello ${this.firstName} ${this.lastName} on coder`);
+}
+let users={
+    firstName:"Drapan",
+    lastName:"Patel"
 }
 
-mainPlane.book(553,"Darpan");
-console.log(mainPlane);
-
-let childplane = {
-    airline:"CHild Plain",
-    iatacode:"cp",
-    bookings:[],
-}
-
-let book = mainPlane.book;
-
-book.call(childplane,689,"key");
-
+let greets = greet.bind(users);
+greets()
