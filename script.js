b68d7cce90d922a8bde09ab5aca4500e08393c99
Darpan1211 <darpan.innovate@gmail.com>
2023-06-16 16:01:52 +0530

    object inside array

--- script.js
+++ script.js
@@ -1,15 +1,12 @@
-// Method--> It is nothing but object property(key) holding 
-//Function as "Value"
+//object inside array
 
-let person = {
-    firstName: 'Darpan',
-    lastName: 'Patel',
-    City:'Surat',
-    birthyear: 1997,
-    Education: 'Computer Science',
-    getSummary: function(){
-        return `${this.firstName} brook lived in ${this.City}.He is ${this.Education} and his birthyear is ${this.birthyear}`;
-    }
+let blocklist = [{userName:"darpan",reson:"Abusive content"},{userName:"Rahul",reson:"avoid content"}]; //
 
+console.log(blocklist);
+
+for(let i = 0; i < blocklist.length; i++){
+    console.log(blocklist[i].userName);
+
+
+    
 }
-console.log(person.getSummary());
