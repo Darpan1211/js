c84fce09911d31606a070305ea6c2e41060bcb1d
Darpan1211 <darpan.innovate@gmail.com>
2023-06-20 13:14:50 +0530

    Buetyfy application create

---
+++ project.js
@@ -0,0 +1,11 @@
+
+//Buetyfy application create
+let buety = document.querySelector(".buety");
+
+let lower = function(){
+    buety.innerHTML=buety.innerHTML.toLowerCase();
+}
+
+let upper = function(){
+    buety.innerHTML=buety.innerHTML.toUpperCase();
+}
