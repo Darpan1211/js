b5b59427858e39e3ac4780077d462e2128154190
Darpan1211 <darpan.innovate@gmail.com>
2023-06-22 15:00:38 +0530

    print oop with using name

--- project.js
+++ project.js
@@ -6,8 +6,10 @@ class Student{
         this.age = age;
 
     }
+    get description(){
+        return `Student ${this.name} has live in ${this.city} and his age is ${this.age}`
+    }
 }
 let student1 = new Student("darpan","Surat","23");
 let student2 = new Student("Eshan","Surat","19");
-console.log(student1);
-console.log(student2);
+console.log(student1.description);
